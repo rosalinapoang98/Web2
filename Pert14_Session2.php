00d330b7c91f695d0f260f6c42d75620a36fb8c8
rosalinapoang <80106475+rosalinapoang@users.noreply.github.com>
2021-06-16 16:01:38 +0700

    Add files via upload

---
+++ Pert14_Session2.php
@@ -0,0 +1,16 @@
+<?php
+/***************************************************
+Halaman ini merupakan contoh halaman pemeriksaan session. Pemeriksaan session biasanya
+dilakukan jika suatu halaman memiliki akses terbatas. Misalnya harus login terlebih dahulu.
+****************************************************/ 
+session_start();
+if (isset($_SESSION['login'])) {
+echo "<h1>Selamat Datang ". $_SESSION['login'] ."</h1>";
+echo "<h2>Halaman ini hanya bisa diakses jika Anda sudah login</h2>";
+echo "<h2>Klik <a href='Pert14_Session3.php'>di sini
+(Pert14_Session3.php)</a> untuk LOGOUT</h2>";
+} else {
+die ("Anda belum login! Anda tidak berhak masuk ke halaman
+ini.Silahkan login <a href='Pert14_Session1.php'>di sini</a>");
+}
+?> 
